ffda730cb11a6289c5a2d78f5a66d5c1040a692c
Andrew Vasquez <andrewvasquez78@yahoo.com>
2022-10-06 11:30:31 -0500

    Taking out p element and container classname

--- summoner-app/src/components/Header.js
+++ summoner-app/src/components/Header.js
@@ -1,9 +1,9 @@
 
 const Header = () => {
   return (
-    <header class="bg-red-500 p-4 mb-10 w-full">
-      <div class="max-w-5xl m-auto">
-        <div class="text-center text-xl font-bold text-white">
+    <header className="bg-red-500 p-4 mb-10 w-full">
+      <div className="max-w-5xl m-auto">
+        <div className="text-center text-xl font-bold text-white">
           League of Legends Summoner Stats
         </div>
       </div>
