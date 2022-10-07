5e9b6714029812ace11146a0ea604d03ee16ace5
Andrew Vasquez <andrewvasquez78@yahoo.com>
2022-10-07 11:37:42 -0500

    got summoner id from api call

--- summoner-app/src/App.js
+++ summoner-app/src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Form from "./components/Form";
+import Form, { playerData } from "./components/Form";
 import logo from "./imgs/logo.png";
 
 
 function App() {
- 
+
+
   return (
     <div>
       <Header />
       <main>
         <div className="flex flex-col-reverse align-middle justify-center m-auto md:max-w-4xl md:pt-16">
           <div className="w-full md:first-letter:w-2/3 mr-24 md:pl-16">
-            <h1 className="text-3xl font-bold mb-5 md:text-4xl md:pl-4">
+            <h1 className="text-3xl pl-8 font-bold mb-5 md:text-4xl md:pl-4">
               Type your Summoner Name to get stats!
             </h1>
-           <img className="mx-auto md:pr-16" src={logo} alt="" srcset="" />
+            <img className="mx-auto md:pr-16" src={logo} alt="Teemo Logo" />
             <Form />
-            
           </div>
-          
         </div>
         <div className="container">
+         
         </div>
       </main>
     </div>
--- summoner-app/src/components/Form.js
+++ summoner-app/src/components/Form.js
@@ -3,46 +3,61 @@ import axios from "axios";
 
 
 
-const Form = () => {
+
+const Form = ({ summonerId }) => {
   const [summonerSearchText, setSummonerSearchText] = useState("");
-  const API_KEY  = process.env.REACT_APP_API_KEY;
+  const API_KEY = process.env.REACT_APP_API_KEY;
   const [playerData, setPlayerData] = useState({});
-//  console.log(summonerSearchText)
- function searchForPlayer(event){
-  event.preventDefault()
-  
-  const APIUrlCall = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerSearchText}?api_key=${API_KEY}`
-   axios.get(APIUrlCall).then(function (response){
-     setPlayerData(response.data)
-     console.log(playerData)
-   }).catch(function (err){
-    console.log(err)
-   })
- }
- 
-  return (
-     <div>
-      <div className="">
-    <form id="formSubmission" className="mt-4 px-6" onSubmit={e => searchForPlayer(e)}>
-      <input
-        type="text"
-        placeholder="Summoner Name"
-        className="w-full md:w-4/5 border-2 bordercolor-gray-200 rounded p-3 text-gray-dark mr-2 focus:outline-none mb-5"
-        onChange={e => setSummonerSearchText(e.target.value)}
-      ></input>
-      <button
-        className="bg-red-500 rounded w-full md:w-4/5  text-white py-3 md:px-4 mt-3 hover:bg-gray-600"
-        type="submit" 
-      >
-        Search
-      </button>
-      
-    </form>
-    </div>
+  //  console.log(summonerSearchText)
+  function searchForPlayer(event) {
+    event.preventDefault();
+
+    const APIUrlCall = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerSearchText}?api_key=${API_KEY}`;
+    
    
+    axios
+      .get(APIUrlCall)
+      .then(function (response) {
+        setPlayerData(response.data);
+        console.log(playerData);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+      
+      summonerId = JSON.stringify(playerData)
+      
+  }
+
+  return (
+    <div>
+      <div className="">
+        <form
+          id="formSubmission"
+          className="mt-4 px-6 md:pl-16"
+          onSubmit={(e) => searchForPlayer(e)}
+        >
+          <input
+            type="text"
+            placeholder="Summoner Name"
+            className="w-full md:w-4/5 border-2 bordercolor-gray-200 rounded p-3 text-gray-dark mr-2 focus:outline-none mb-5"
+            onChange={(e) => setSummonerSearchText(e.target.value)}
+          ></input>
+          <button
+            className="bg-red-500 rounded w-full md:w-4/5  text-white py-3 md:px-4 mt-3 hover:bg-gray-600"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+      <div>
+
+      </div>
+
+
     </div>
   );
 };
 
 export default Form;
-
