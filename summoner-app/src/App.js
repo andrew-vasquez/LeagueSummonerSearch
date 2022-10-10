b27e5a72ce135a9bfc4b52fa68f72cf4f350f619
Andrew Vasquez <andrewvasquez78@yahoo.com>
2022-10-09 21:40:23 -0500

    same message

--- summoner-app/src/App.js
+++ summoner-app/src/App.js
@@ -2,12 +2,9 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
-import logo from "./components/logo.png"
+import logo from "./components/logo.png";
 
-
-
-function App() {
-
+function App () {
 
   return (
     <div className="bg-mainBackground font-sans">
@@ -15,14 +12,11 @@ function App() {
       <main>
         <div className="flex flex-col-reverse align-middle justify-center m-auto md:max-w-4xl md:pt-16">
           <div id className="w-full md:first-letter:w-2/3 mr-24 md:pl-16">
-             <h1 className="text-white text-3xl pl-8 font-bold mb-5 md:text-4xl md:pl-4">
-              Type your Summoner Name to get stats!
-            </h1>
-            <img className="mx-auto " src={logo} alt="Teemo Logo" />
+              
+              <img className="mx-auto " src={logo} alt="Teemo Logo" />
             <Form />
           </div>
         </div>
-        
       </main>
     </div>
   );
